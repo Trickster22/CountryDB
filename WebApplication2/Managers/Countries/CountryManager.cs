865fb410a1811f24f7d4b5d800d7ebb1bed92bbd
Tr1ckster <m.s.alexandroff@yandex.ru>
2020-10-13 12:54:26 +0300

    ничего не поменялось

--- WebApplication2/Managers/Countries/CountryManager.cs
+++ WebApplication2/Managers/Countries/CountryManager.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+//gjndfdsbfdsb
 namespace CountryProject.Managers.Countries
 {
     public class CountryManager : ICountryManager
